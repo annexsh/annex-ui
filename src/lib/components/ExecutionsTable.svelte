99aa1ab9d95bc9dfd4dfffa7668ddfd7e4e9a1bf
Josh Jon <joshjon.dev@gmail.com>
2024-10-18 17:09:21 +1100

    Add not found messages (#5)

--- src/lib/components/ExecutionsTable.svelte
+++ src/lib/components/ExecutionsTable.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+	import { Button, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
 	import { getExecutionStatus } from '$lib/models/execution';
 	import { type PartialMessage, Timestamp } from '@bufbuild/protobuf';
 	import More from '$lib/components/widgets/More.svelte';
@@ -7,9 +7,13 @@
 	import ExecutionStatusBadge from '$lib/components/ExecutionStatusBadge.svelte';
 	import { testExecutions } from '$lib/stores/test-executions';
 	import { TestExecution } from '@annexsh/annex-proto/gen/annex/tests/v1/test_pb';
+	import { PlusOutline } from 'flowbite-svelte-icons';
 
+	export let open: boolean;
 	export let context: string;
 	export let testSuiteId: string;
+
+	const columns = ['ID', 'Scheduled', 'Started', 'Finished', 'Status', ''];
 
 	function dateFromPartialTS(ts: PartialMessage<Timestamp> | undefined): string {
 		if (!ts) {
@@ -21,29 +25,44 @@
 
 <Table>
 	<TableHead class="sticky top-0">
-		{#each ['ID', 'Scheduled', 'Started', 'Finished', 'Status', ''] as title}
+		{#each columns as title}
 			<TableHeadCell class="p-4 font-medium">{title}</TableHeadCell>
 		{/each}
 	</TableHead>
 	<TableBody>
-		{#each $testExecutions as execution}
-			{@const status=getExecutionStatus(new TestExecution(execution))}
-			<TableBodyRow class="text-base">
-				<TableBodyCell class="p-4">{execution.id}</TableBodyCell>
-				{#each [execution.scheduleTime, execution.startTime, execution.finishTime] as ts}
-					<TableBodyCell class="px-4 font-normal text-gray-500 dark:text-gray-400">
-						{dateFromPartialTS(ts)}
-					</TableBodyCell>
-				{/each}
-				<TableBodyCell class="p-4 font-normal">
-					<div class="flex items-center gap-2">
-						<ExecutionStatusBadge status={status} />
+		{#if $testExecutions.length === 0}
+			<tr>
+				<td colspan={columns.length}>
+					<div class="text-base text-center items-center text-gray-500 my-20">
+						<p class="mb-5">No test executions found</p>
+
+						<Button color="alternative" on:click={() => {open = true}} size="md" class="gap-2 whitespace-nowrap px-3">
+							<PlusOutline size="md" />
+							New Execution
+						</Button>
 					</div>
-				</TableBodyCell>
-				<TableBodyCell class="p-4">
-					<More href={testExecutionRoute(context, testSuiteId, execution.testId, execution.id)} />
-				</TableBodyCell>
-			</TableBodyRow>
-		{/each}
+				</td>
+			</tr>
+		{:else}
+			{#each $testExecutions as execution}
+				{@const status=getExecutionStatus(new TestExecution(execution))}
+				<TableBodyRow class="text-base">
+					<TableBodyCell class="p-4">{execution.id}</TableBodyCell>
+					{#each [execution.scheduleTime, execution.startTime, execution.finishTime] as ts}
+						<TableBodyCell class="px-4 font-normal text-gray-500 dark:text-gray-400">
+							{dateFromPartialTS(ts)}
+						</TableBodyCell>
+					{/each}
+					<TableBodyCell class="p-4 font-normal">
+						<div class="flex items-center gap-2">
+							<ExecutionStatusBadge status={status} />
+						</div>
+					</TableBodyCell>
+					<TableBodyCell class="p-4">
+						<More href={testExecutionRoute(context, testSuiteId, execution.testId, execution.id)} />
+					</TableBodyCell>
+				</TableBodyRow>
+			{/each}
+		{/if}
 	</TableBody>
 </Table>
--- src/routes/(app)/contexts/[context]/test-suites/+page.svelte
+++ src/routes/(app)/contexts/[context]/test-suites/+page.svelte
@@ -3,6 +3,7 @@
 	import { TestSuite } from '@annexsh/annex-proto/gen/annex/tests/v1/test_pb.js';
 	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
 	import { page } from '$app/stores';
+	import ItemsNotFoundMessage from '$lib/components/ItemsNotFoundMessage.svelte';
 
 	export let data;
 
@@ -17,11 +18,16 @@
 	</Breadcrumb>
 
 	<div class="mt-px space-y-4">
-		<div class="grid grid-cols-1 gap-4 xl:grid-cols-3">
-			{#each testSuites as testSuite}
-				<TestSuiteCard testSuite={new TestSuite(testSuite)} />
-			{/each}
-		</div>
+		{#if !testSuites.length}
+			<ItemsNotFoundMessage title="No Test Suites found"
+														message="Get started by registering a Test Suite Runner using one of the supported SDKs." />
+		{:else }
+			<div class="grid grid-cols-1 gap-4 xl:grid-cols-3">
+				{#each testSuites as testSuite}
+					<TestSuiteCard testSuite={new TestSuite(testSuite)} />
+				{/each}
+			</div>
+		{/if}
 	</div>
 </main>
 
